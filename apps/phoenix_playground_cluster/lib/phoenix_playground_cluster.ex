6beb09c756732aadd636c7117b29de5fa3dd88cb
twozeronine <amdusias209@gmail.com>
2023-02-08 21:43:22 +0900

    feat : plug libcluster & remove annotation

--- apps/phoenix_playground_cluster/lib/phoenix_playground_cluster.ex
+++ apps/phoenix_playground_cluster/lib/phoenix_playground_cluster.ex
@@ -1,18 +1,3 @@
 defmodule PhoenixPlaygroundCluster do
-  @moduledoc """
-  Documentation for `PhoenixPlaygroundCluster`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PhoenixPlaygroundCluster.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- apps/phoenix_playground_cluster/test/phoenix_playground_cluster_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PhoenixPlaygroundClusterTest do
-  use ExUnit.Case
-  doctest PhoenixPlaygroundCluster
-
-  test "greets the world" do
-    assert PhoenixPlaygroundCluster.hello() == :world
-  end
-end
